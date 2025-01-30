c80ad84f8f97bf6d44270736d0c343947e0f829d
Austin Tollenaar <85308338+athonix-frontend@users.noreply.github.com>
2025-01-30 14:17:05 -0800

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,12 +1,16 @@
 from sqlalchemy.orm import Session
 from app.models import User
 from app.schemas import UserCreate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
 def create_user(db: Session, user: UserCreate):
-    db_user = User(name=user.name, email=user.email)
+    hashed_password = pwd_context.hash(user.password)  # Hash password
+    db_user = User(name=user.name, email=user.email, password=hashed_password, google_oauth=user.google_oauth)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
